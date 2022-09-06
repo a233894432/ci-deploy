6ed20a52cc32963ed97251d4389f81f960300758
diogoxiang <diogoxiang@gmail.com>
2022-09-06 14:45:20 +0800

    test. ci

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function Home() {
     <div>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>当前在 Home 页面</h1>
+        <h1>当前在 Home 页面 test ci / cd </h1>
         <div>TEST CICD production</div>
         <Link to="/about" className="App-link">About</Link>
       </header>
